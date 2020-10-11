b3f2846be7a07124c7a3ef5d60cd566bd9b5b9d6
xiongyuanbiao <2464985918@qq.com>
2020-10-11 09:59:41 +0800

    添加了退出功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,41 @@
 $(function() {
     getUserInfo()
+    var layer = layui.layer
+
+    //退出功能
+    $('#closeindex').on('click', function() {
+        layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function(index) {
+            //清空本地储存中的koken防止被盗用
+            localStorage.removeItem('token')
+                //重新跳转到登录页面
+            location.href = './login.html'
+                //关闭询问框
+            layer.close(index)
+        })
+    })
+
 
     function getUserInfo() {
         $.ajax({
             method: 'GET',
             url: '/my/userinfo',
-            headers: {
-                Authorization: localStorage.getItem('token') || ''
-            },
+            // headers: {
+            //     Authorization: localStorage.getItem('token') || ''
+            // },
             success: function(res) {
-                console.log(res);
-                console.log(res.data);
+                // console.log(res);
+                // console.log(res.data);
                 if (res.status !== 0) {
                     return layui.layer.msg('获取用户信息失败！')
                 }
                 renderAvatar(res.data)
+            },
+            complete: function(res) {
+                if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                    localStorage.removeItem('token')
+                    location.href = '/login.html'
+                }
+
             }
         })
     }
